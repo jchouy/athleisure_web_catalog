b0817a389b06a7b4d36128080eb282892eb0a0f8
Jessica Chouy <jchouy@theknotww.com>
2023-07-10 15:59:42 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 # put the data into a dataframe
-df - pandas.DataFrame(my_catalog)
+df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page to check data
 # streamlit.write(df) 
